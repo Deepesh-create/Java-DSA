36391f3218f1969be1173d76cfd39596ea064f3a
Deepesh <73437033+Deepesh-create@users.noreply.github.com>
2026-09-24 23:56:48 +0530

    Day-2 (Q-9)
    Implement twoSum method to find indices of two numbers that add up to the target in a sorted array.

---
+++ Day-2/167. Two Sum II - Input Array Is Sorted.java
@@ -0,0 +1,25 @@
+// Day-2 (Q-9) https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        
+        int i = 0;
+        int j = numbers.length - 1;
+
+        while(i<j){
+           int sum = numbers[i] + numbers[j];
+
+            if(sum > target){
+                j--;
+            }
+            else if(sum < target){
+                i++;
+            }
+            else{
+                return new int[]{i+1,j+1};
+            }
+
+        }
+        return new int[]{-1,-1};
+    }
+}
